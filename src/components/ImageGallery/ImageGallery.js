b0fe56dc1a79351a9935227ebf177ed669df1bf5
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-09-18 20:01:41 +0300

    draft hooks created

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react';
+import {useState, createRef} from 'react';
 import toast from 'react-hot-toast';
 import PropTypes from 'prop-types';
 
@@ -9,99 +9,103 @@ import SpinnerLoader from '../Loader';
 import { fetchImages } from '../../services/images-api';
 import {List} from './ImageGallery.styled.jsx'
 
-export default class ImageGallery extends Component {
+export default function ImageGallery({ searchName, onSelect }) {
+    const [images, setImages] = useState(null);
+    const [error, setError] = useState(null);
+    const [page, setPage] = useState(1);
+    const [searchResult, setSearchResult] = useState(null);
+    const [status, setStatus] = useState('idle');
+    const [spinner, setSpinner] = useState(false);
 
-    state = {
-        images: null,
-        error: null,
-        page: 1,
-        searchResult: null,
-        status: 'idle',
-        spinner: false
-    }
+    const myRef = createRef();
 
-    myRef = createRef();
-
-    componentDidUpdate(prevProps, prevState) {
+    function componentDidUpdate(prevProps, prevState) {
         const prevName = prevProps.searchName;
-        const nextName = this.props.searchName;
+        const nextName = searchName;
         const prevPage = prevState.page;
-        const nextPage = this.state.page;
-        const images = this.state.images;
+        const nextPage = page;
+        const gallery = images;
 
         if (prevPage < nextPage) {
-            this.setState({spinner:true});
+            setSpinner(true);
             fetchImages(nextName, nextPage)
-            .then((data) => {
-                let newImages = [...images, ...data.hits];
-                this.setState({ images: newImages, status: 'resolved', spinner:false });
-                this.scrollToRef();
-            })
-                .catch(error => this.setState({ error, status: 'rejected', spinner:false }))
-            ;
+                .then((data) => {
+                    let newGallery = [...gallery, ...data.hits];
+                    setStatus('resolved');
+                    setSpinner(false);
+                    setImages(newGallery);
+                    scrollToRef();
+                })
+                .catch(error => {
+                    setError(error);
+                    setStatus('rejected');
+                    setSpinner(false);
+                })
+                ;
         }
 
         if (prevName !== nextName) {
-            this.setState({ spinner:true, page:1 });
+            this.setState({ spinner: true, page: 1 });
             fetchImages(nextName, 1)
                 .then((data) => {
-                    this.setState({ images: data.hits, searchResult: data.total, status: 'resolved', spinner: false });
-                    if (this.state.images.length === 0) {
-                    return toast('Извините, по вашему запросу ничего не найдено', {style: {
-                                borderRadius: '10px',
-                                background: '#333',
-                                color: '#fff',
-                    },});
-                }
-            })
-            .catch (error => this.setState({ error, status: 'rejected' }))
-        }
-    }
-
-    toggleLoadMore = () => {
-        this.setState({ spinner:true, page: this.state.page + 1 });
-    }
-
-    scrollToRef = () => {
-        this.myRef.current.scrollIntoView({behavior: "smooth", block: "end"});
-    }
-    
-    render() {
-        const { images, status, searchResult, spinner } = this.state;
-
-        if (status === 'idle') {
-            return <div></div>
-        }
-
-        if (status === 'rejected') {
-            return toast('Извините, по вашему запросу ничего не найдено', {style: {
+                    setStatus('resolved');
+                    setSpinner(false);
+                    setImages(data.hits);
+                    setSearchResult(data.total);
+                    
+                    if (images.length === 0) {
+                        return toast('Извините, по вашему запросу ничего не найдено', {
+                            style: {
                                 borderRadius: '10px',
                                 background: '#333',
                                 color: '#fff',
                             },
-            });
-        }
-
-        if (status === 'resolved') {
-            return (
-                <div>
-                    <List >
-                        {images.map(image=> (
-                            <ImageGalleryItem src={image.webformatURL} alt={image.tags} onClick={() => this.props.onSelect(image.largeImageURL)} key={image.id}/>
-                        ))}
-                    </List>
-                    {spinner && <SpinnerLoader /> }
-                    <div ref={this.myRef} >{searchResult>12 && <LoadMoreButton onClick={this.toggleLoadMore} />}</div>
-                </div>
-                
-            )
+                        });
+                    }
+                })
+                .catch(error => {
+                    setError(error);
+                    setStatus('rejected');
+                })
         }
     }
+
+    const toggleLoadMore = () => {
+        setSpinner(true);
+        setPage(page + 1)
+    }
+
+    const scrollToRef = () => {
+        myRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+
+    if (status === 'idle') {
+        return <div></div>
+    }
+    
+    if (status === 'rejected') {
+        return toast('Извините, по вашему запросу ничего не найдено', {
+            style: {
+                borderRadius: '10px',
+                background: '#333',
+                color: '#fff',
+            },
+        });
+    }
+    
+    if (status === 'resolved') {
+        return (
+            <div>
+                <List >
+                    {images.map(image => (
+                        <ImageGalleryItem src={image.webformatURL} alt={image.tags} onClick={() => onSelect(image.largeImageURL)} key={image.id} />
+                    ))}
+                </List>
+                {spinner && <SpinnerLoader />}
+                <div ref={myRef} >{searchResult > 12 && <LoadMoreButton onClick={toggleLoadMore} />}</div>
+            </div>
+                
+        )
+    
+    }
 }
-
-ImageGallery.propTypes = {
-    onSubmit: PropTypes.func,
-    onSelect: PropTypes.func,
-    searchName: PropTypes.string.isRequired,
-};
-
