0ec9b049f1ed68c20d24df648f299a50dab1de40
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-09-26 12:47:13 +0300

    app refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
+import toast from 'react-hot-toast';
 import { Toaster } from 'react-hot-toast';
 import Modal from '../Modal';
 import Searchbar from '../Searchbar';
 import ImageGallery from '../ImageGallery';
-import {Container} from './App.styled'
+import { Container } from './App.styled'
+import { fetchImages } from '../../services/images-api';
+import LoadMoreButton from '../Button';
+import SpinnerLoader from '../Loader';
+
+const Status = {
+    IDLE: 'idle',
+    RESOLVED: 'resolved',
+    REJECTED:'rejected'
+}
 
 export default function App() {
   
   const [searchName, setSearchName] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
- 
+  const [images, setImages] = useState(null);
+  const [page, setPage] = useState(null);
+  const [searchResult, setSearchResult] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [spinner, setSpinner] = useState(false);
+
+  const myRef = useRef();
+
   const handleSearchSubmit = (searchName) => {
     setSearchName(searchName);
+    setPage(1);
+    setImages([]);
   }
 
+  useEffect(() => {
+    fetchImages(searchName, page)
+      .then((data) => {
+        if (data.hits.length === 0) {
+          return toast('Извините, по вашему запросу ничего не найдено', {
+            style: {
+              borderRadius: '10px',
+              background: '#333',
+              color: '#fff',
+            },
+          });
+        }
+        let newImages = [...images, ...data.hits];
+        setImages(newImages);
+
+        setSearchResult(data.total);
+        setStatus(Status.RESOLVED);
+        setSpinner(false);
+        scrollToRef();
+      })
+
+      .catch(error => {
+        setStatus(Status.REJECTED);
+        setSpinner(false);
+      });
+
+  }, [searchName, page])
+
+    
   const handleSelectImage = imageUrl => {
     setSelectedImage(imageUrl);
   }
+
+  const scrollToRef = () => {
+        myRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
+    
+  const toggleLoadMore = () => {
+        setSpinner(true);
+        setPage(page => page + 1);
+    }
 
   const closeModal = () => setSelectedImage(null);
 
@@ -25,9 +82,12 @@ export default function App() {
       <Container>
         
           <Searchbar onSubmit={handleSearchSubmit} />
-        <ImageGallery
-          searchName={searchName}
-          onSelect={handleSelectImage} />
+          <ImageGallery
+            images={images}
+            status={status}
+            onSelect={handleSelectImage} />
+          {spinner && <SpinnerLoader />}
+          <div ref={myRef} >{searchResult > 12 && <LoadMoreButton onClick={toggleLoadMore} />}</div>
           {selectedImage && <Modal onClose = {closeModal}>
             <img src={selectedImage} alt="" />
           </Modal>}
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,12 +1,6 @@
-import {useState, useEffect, useRef} from 'react';
 import toast from 'react-hot-toast';
 import PropTypes from 'prop-types';
-
 import ImageGalleryItem from '../ImageGalleryItem';
-import LoadMoreButton from '../Button';
-import SpinnerLoader from '../Loader';
-
-import { fetchImages } from '../../services/images-api';
 import { List } from './ImageGallery.styled.jsx'
 
 const Status = {
@@ -15,77 +9,7 @@ const Status = {
     REJECTED:'rejected'
 }
 
-export default function ImageGallery({ searchName, onSelect}) {
-    const [images, setImages] = useState( []);
-    const [page, setPage] = useState( 1 );
-    const [searchResult, setSearchResult] = useState(null);
-    const [status, setStatus] = useState(Status.IDLE);
-    const [spinner, setSpinner] = useState(false);
-
-    const myRef = useRef();
-    
-    //new search
-     useEffect(() => {
-        if (!searchName) {
-            return; 
-        }
-        setSpinner(true);
-    
-        fetchImages(searchName, 1)
-            .then((data) => {
-                if (data.hits.length === 0) {
-                    return toast('Alas, no items found per your query', {
-                        style: {
-                            borderRadius: '10px',
-                            background: '#333',
-                            color: '#fff',
-                        },
-                    });
-                }
-                setImages(data.hits);
-                setSearchResult(data.total);
-                setStatus(Status.RESOLVED);
-                setSpinner(false);
-                scrollToRef();
-            })
-            .catch(error => {
-                setStatus(Status.REJECTED);
-                setSpinner(false);
-            });
-        
-     }, [searchName])
-    
-    //load more
-    useEffect(() => {
-        if (!searchName) {
-            return; 
-        }
-        setSpinner(true);
-        fetchImages(searchName, page)
-            .then((data) => {
-                let newImages = [...images, ...data.hits];
-                setImages(newImages);
-                setSearchResult(data.total);
-                setStatus(Status.RESOLVED);
-                setSpinner(false);
-                scrollToRef();
-            })
-            .catch(error => {
-                setStatus(Status.REJECTED);
-                setSpinner(false);
-            });
-        
-    },[page])
-
-    const toggleLoadMore = () => {
-        setSpinner(true);
-        setPage(page => page + 1);
-    }
-
-    const scrollToRef = () => {
-        myRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
-    }
-
+export default function ImageGallery({ images, status, onSelect}) {
     if (status === Status.IDLE) {
         return <div></div>
     }
@@ -108,17 +32,12 @@ export default function ImageGallery({ searchName, onSelect}) {
                         <ImageGalleryItem src={image.webformatURL} alt={image.tags} onClick={() => onSelect(image.largeImageURL)} key={image.id} />
                     ))}
                 </List>
-                {spinner && <SpinnerLoader />}
-                <div ref={myRef} >{searchResult > 12 && <LoadMoreButton onClick={toggleLoadMore} />}</div>
-            </div>
-                
+            </div> 
         )
-    
     }
 }
 
 ImageGallery.propTypes = {
     onSubmit: PropTypes.func,
     onSelect: PropTypes.func,
-    searchName: PropTypes.string.isRequired,
 };
